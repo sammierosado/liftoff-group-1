d0647cbb354f9a255a2255b5cef3661e7d3d1454
bahbond <bondopicks@gmail.com>
2023-12-05 14:27:43 -0600

    Initialize Spring

--- VenuApp/src/main/java/infinitycodecrew/VenuApp/controllers/WelcomeController.java
+++ VenuApp/src/main/java/infinitycodecrew/VenuApp/controllers/WelcomeController.java
@@ -8,11 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 
 public class WelcomeController {
-
-
-
-
-   @GetMapping("/")
+    @GetMapping("/")
     public String welcome(){
         return "welcome/gnr";
     }
