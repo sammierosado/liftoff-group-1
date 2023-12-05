376b06c6be3e1365281f68787d314fe03d19d794
bahbond <bondopicks@gmail.com>
2023-12-05 13:53:07 -0600

    initialized springboot with sql

---
+++ VenuApp/src/main/java/infinitycodecrew/VenuApp/VenuAppApplication.java
@@ -0,0 +1,13 @@
+package infinitycodecrew.VenuApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenuAppApplication {
+
+	public static void main(String[] args) {SpringApplication.run(VenuAppApplication.class, args);
+
+	}
+
+}
---
+++ VenuApp/src/main/java/infinitycodecrew/VenuApp/controllers/WelcomeController.java
@@ -0,0 +1,19 @@
+package infinitycodecrew.VenuApp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+
+public class WelcomeController {
+
+
+
+
+   @GetMapping("/")
+    public String welcome(){
+        return "welcome/gnr";
+    }
+}
