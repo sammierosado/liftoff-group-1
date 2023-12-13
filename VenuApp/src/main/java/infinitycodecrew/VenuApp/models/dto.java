a70066755b03a7d461d979028adba5c2613638a5
malwaredb86 <mdurham86@gmail.com>
2023-12-12 20:42:25 -0500

    Created Authentication Controller. Login and Register forms now functional

--- VenuApp/src/main/java/infinitycodecrew/VenuApp/models/dto.java
+++ VenuApp/src/main/java/infinitycodecrew/VenuApp/models/dto.java
@@ -1,2 +1,45 @@
-package infinitycodecrew.VenuApp.models;public class dto {
+package infinitycodecrew.VenuApp.models;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class dto {
+
+    public static class LoginFormDTO {
+
+        @NotNull
+        @NotBlank
+        @Size(min=6, max=20, message = "Invalid username! Must be between 6 and 20 characters.")
+        private String username;
+
+        @NotNull
+        @NotBlank
+        @Size(min=10, max=30, message= "Invalid password! Must be between 10 and 30 characters.")
+        private String password;
+
+        public String getUsername() {return username;}
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {return password;}
+
+        public void setPassword(String password){
+            this.password = password;
+        }
+    }
+
+    public static class RegisterFormDTO extends LoginFormDTO {
+        private String verifyPassword;
+
+        public String getVerifyPassword() {return verifyPassword;}
+
+        public void setVerifyPassword(String verifyPassword) {
+            this.verifyPassword=verifyPassword;
+        }
+    }
+
+
 }
