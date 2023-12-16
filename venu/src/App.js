a15a312197aafe76a1de82b316b3d13825c019c5
Jay <138824448+jaythemself@users.noreply.github.com>
2023-12-16 15:33:40 -0600

    very basic Google Map is up. added a soundwave icon for browser tab to images folder

--- venu/src/App.js
+++ venu/src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
+import {useJsApiLoader, GoogleMap} from '@react-google-maps/api';
+
+const mapCenter = {lat: 38.627003, lng: -90.199402}
 
 function App() {
+  const {isLoaded} = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+  })
+  if (!isLoaded) return <div>Loading...</div>;
+
   return (
     <div className="App" >
       <header className="App-header">
         
         {/* topmenu is basic nav bar at top of screen
-            rest of header is default react populated. left in for now in case anyone has a use for the template. */}
+            rest of header is default react populated. left in -for now- in case anyone has a use for the link template. */}
         <ul class="topmenu">
           <li><a href="#home" class="active">Home</a></li>
           <li><a href="#createevent">Create Event</a></li>
@@ -32,9 +40,8 @@ function App() {
       {/* google map */}
       <div className="VENU-map">
         <box>
-          <div class="overlay">
-            {/* TODO map overlay */}
-          </div>
+          {/*do i need to set box size/position etc in css/name the box to reference there? brush up x1000000 */}
+            <GoogleMap center={mapCenter} zoom={10} mapContainerStyle={{width: '100%', height: '100%'}} />
         </box>
       </div>
 
