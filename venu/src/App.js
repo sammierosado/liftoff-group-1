a87dfc042e97716b882eb9eac1bd0cc2761b5ffc
Jay <138824448+jaythemself@users.noreply.github.com>
2023-12-16 21:33:48 -0600

    added map, cleaned up code and added doc comments, worked on pages and nav bar

--- venu/src/App.js
+++ venu/src/App.js
@@ -1,52 +1,18 @@
-import './App.css';
-import {useJsApiLoader, GoogleMap} from '@react-google-maps/api';
-
-const mapCenter = {lat: 38.627003, lng: -90.199402}
+import * as Pages from './pages';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const {isLoaded} = useJsApiLoader({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
-  })
-  if (!isLoaded) return <div>Loading...</div>;
-
+  
+  // Adds ability to path to different pages, just add Route here and info in index.js in src/pages folder.
   return (
-    <div className="App" >
-      <header className="App-header">
-        
-        {/* topmenu is basic nav bar at top of screen
-            rest of header is default react populated. left in -for now- in case anyone has a use for the link template. */}
-        <ul class="topmenu">
-          <li><a href="#home" class="active">Home</a></li>
-          <li><a href="#createevent">Create Event</a></li>
-          <li><a href="#venues">Venues</a></li>
-          <li><a href="#about">About Us</a></li>
-        </ul>
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-      </header>
-
-      {/* google map */}
-      <div className="VENU-map">
-        <box>
-          {/*do i need to set box size/position etc in css/name the box to reference there? brush up x1000000 */}
-            <GoogleMap center={mapCenter} zoom={10} mapContainerStyle={{width: '100%', height: '100%'}} />
-        </box>
-      </div>
-
-    </div>
+    <Router>          
+     <Routes>
+        <Route path="/" element={<Pages.Home />} />
+        <Route path="/about" element={<Pages.About />} />
+      </Routes>    
+    </Router>
   );
+ 
 }
 
 export default App;
---
+++ venu/src/pages/Home.js
@@ -0,0 +1,32 @@
+import './stylesheets/Home.css';
+import {useJsApiLoader, GoogleMap} from '@react-google-maps/api';
+import Navbar from '../components/Navbar';
+
+// geographical location on which the Google map is centered. feel free to change it!
+const mapCenter = {lat: 38.627003, lng: -90.199402}
+
+function Home() {
+
+    // checking if the API key is loading properly. y'all will need your own in your .env.local file but I will walk you through it!!
+    const {isLoaded} = useJsApiLoader({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+      })
+      if (!isLoaded) return <div>Loading...</div>;
+
+    return (
+        <div>
+          <Navbar/> 
+          <div className="Home" >
+
+          {/* google map information */}
+          <div className="VENU-map">
+              {/* may end up separating out parameters, this was just to make sure it worked correctly */}
+              <GoogleMap center={mapCenter} zoom={10} mapContainerStyle={{width: '100%', height: '100%'}} />
+          </div>
+        
+        </div>
+      </div>
+    );
+}
+
+export default Home;
