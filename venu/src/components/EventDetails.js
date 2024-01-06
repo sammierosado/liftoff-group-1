4a74c125e65f4bde182248d2668486ef040ed4a4
keydamm <keyonamdamm@hotmail.com>
2024-01-05 23:12:45 -0600

    not rendering not sure why

--- venu/src/components/EventDetails.js
+++ venu/src/components/EventDetails.js
@@ -15,17 +15,17 @@ const EventDetails = () => {
         }
 
         const data = await response.json();
-        console.log('Fetched event details:', data);
+//        console.log('Fetched event details:', data);
         setEventDetails(data);
-        console.log('Fetched event details:', eventDetails);
+//        console.log('Fetched event details:', eventDetails);
       } catch (error) {
-        console.error('Error fetching event details:', error);
+//        console.error('Error fetching event details:', error);
       }
     };
 
     fetchEventDetails();
   }, [id]);
-  console.log('Rendered event details:', eventDetails);
+//  console.log('Rendered event details:', eventDetails);
 
   return (
       <div>
