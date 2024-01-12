355dee86c7f62f9434a889e8862ffccf091a8916
malwaredb86 <mdurham86@gmail.com>
2024-01-12 11:41:02 -0500

    Fixed security config by adding cors bean and changing permissions to allow access to pages while not logged in

--- VenuApp/src/main/java/infinitycodecrew/VenuApp/models/data/UserRepository.java
+++ VenuApp/src/main/java/infinitycodecrew/VenuApp/models/data/UserRepository.java
@@ -3,7 +3,6 @@ package infinitycodecrew.VenuApp.models.data;
 
 import infinitycodecrew.VenuApp.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
