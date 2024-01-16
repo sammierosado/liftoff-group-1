1579d64baef2ac336958d97873896727bf959051
keydamm <keyonamdamm@hotmail.com>
2024-01-16 12:09:58 -0600

    forgot to add pathing

--- venu/src/pages/index.js
+++ venu/src/pages/index.js
@@ -2,10 +2,11 @@ import About from "./About";
 import Home from "./Home";
 import Events from "./Events";
 import Venues from "./Venues";
+import Artists from "./Artists";
 import Spotify from "./Spotify"
 import SpotifyLogin from "./SpotifyLogin"
 
 // This is for routing (App.js import Pages) so we won't have to have a million import statements elsewhere.
 // Just import newly created pages with the above format and add them to the export statement.
 
-export {About, Home, Events, Venues, Spotify, SpotifyLogin};
+export {About, Home, Events, Venues, Artists, Spotify, SpotifyLogin};
