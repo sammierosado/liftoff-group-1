c03dc42ab8b06f5bc0b7b31ff0acdaad72537224
Jay <138824448+jaythemself@users.noreply.github.com>
2024-01-16 18:03:05 -0600

    about unfinished/broken just need to switch branches rq

--- venu/src/pages/About.js
+++ venu/src/pages/About.js
@@ -25,7 +25,7 @@ function About() {
                   <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container">
                     <div class="team-card">
                       <div class="img-wrapper">
-                        <img class="team-img" src=".//public/images/chill-dratini-square.jpg">
+                        <img class="team-img" src="./public/images/chill-dratini-square.jpg">
                       </div>
                       <p class="text-blk name">
                         Name 1
@@ -34,17 +34,14 @@ function About() {
                         Title 1
                       </p>
                       <div class="social-media-links">
-                        <a href="http://www.twitter.com/" target="_blank">
-                          <img src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-twitter.svg">
+                        <a href="https://github.com/jaythemself" target="_blank">
+                          <img src="./public/images/icons8-github-squared-100.png">
                         </a>
-                        <a href="http://www.facebook.com/" target="_blank">
-                          <img src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-fb.svg">
-                        </a>
-                        <a href="http://www.instagram.com/" target="_blank">
-                          <img src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-insta.svg">
+                        <a href="https://www.linkedin.com/in/jayhamrick/" target="_blank">
+                          <img src="./public/images/icons8-linkedin-100.png">
                         </a>
                         <a href="http://www.gmail.com/" target="_blank">
-                          <img src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-mail.svg">
+                          <img src="./public/images/icons8-gmail-logo-100.png">
                         </a>
                       </div>
                     </div>
